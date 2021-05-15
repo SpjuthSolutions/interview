1c6c86c4d0336e862483ab013aad125ef2e89ac7
SpjuthSolutions <31391974+SpjuthSolutions@users.noreply.github.com>
2021-05-15 16:33:30 +0200

    Update Mystery.java

--- Mystery.java
+++ Mystery.java
@@ -1,3 +1,9 @@
+//Will the code compile? 
+//What's going to happen? 
+//What's it called when the method calls itself? 
+//What will be the output for every iteration?
+//What's the error that can occur with this method called?
+
 public class Mystery {
 
     public static void main(String[] args) {
